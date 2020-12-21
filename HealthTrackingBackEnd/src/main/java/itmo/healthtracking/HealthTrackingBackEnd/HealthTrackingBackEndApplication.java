31e20265b944d5ccec9ae3d437f8d866e0f598a1
tuandung157 <letuandung157@gmail.com>
2020-12-21 12:13:51 +0300

    init

---
+++ HealthTrackingBackEnd/src/main/java/itmo/healthtracking/HealthTrackingBackEnd/HealthTrackingBackEndApplication.java
@@ -0,0 +1,13 @@
+package itmo.healthtracking.HealthTrackingBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthTrackingBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthTrackingBackEndApplication.class, args);
+	}
+
+}
---
+++ HealthTrackingBackEnd/src/test/java/itmo/healthtracking/HealthTrackingBackEnd/HealthTrackingBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.healthtracking.HealthTrackingBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTrackingBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
